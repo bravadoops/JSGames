3e5af19128fae29769c3caeca5dae56a89ebac48
m <xiaoham3@uci.edu>
2018-07-23 17:05:36 +0800

    complete calculate neighbor bombs

--- MineSweeper/sketch.js
+++ MineSweeper/sketch.js
@@ -6,27 +6,41 @@ function make2DArray(rows, cols) {
     return arr;
 }
 
-function Cell() {
-    this.bee = true;
-}
+
 
 var grid;
 var rows = 20;
 var cols = 20;
+var w = 40;
+
 
 function setup() {
-    createCanvas(200, 200);
+    createCanvas(rows*w+1, cols*w+1);
     grid = make2DArray(rows, cols);
-    for (var i = 0; i < rows; i++) {
+    for (var i = 0; i < rows; i++)
         for (var j = 0; j < cols; j++) {
-            grid[i][j] = new Cell();
-        }
+        grid[i][j] = new Cell(i, j, w);
     }
+    for (var i = 0; i < rows; i++)
+        for (var j = 0; j < cols; j++) {
+            grid[i][j].countNeighbors();
+        }
 }
 
+function mousePressed() {
+    for (var i = 0; i < rows; i++) {
+        for (var j = 0; j < cols; j++) {
+            if (grid[i][j].contains(mouseX, mouseY)) {
+                grid[i][j].reveal();
+            }
+
+        }
+    }
+
+}
 
 function draw() {
-    background(0)
+    background(244);
     for (var i = 0; i < rows; i++) {
         for (var j = 0; j < cols; j++) {
             grid[i][j].show();
@@ -34,4 +48,3 @@ function draw() {
     }
 }
 
-setup();
